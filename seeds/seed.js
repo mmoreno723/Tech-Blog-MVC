68c364b4ed7412740af6c41b3a7cb6812ccd222c
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-22 21:25:52 -0400

    seeds json files added

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,30 @@
 const sequelize = require("../config/connection");
+const Blog = require("../models/Blog");
+const Comment = require("../models/Comment");
+const User = require("../models/User");
+const blogData = require("./blog-seeds.json");
+const commentData = require("./comment-seeds.json");
+const userData = require("./user-seeds.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Blog.bulkCreate(blogData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
