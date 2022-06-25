a8ef95134db9de67c7960f698b04af8e12a20690
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-25 19:46:06 -0400

    handlesbars and server updated

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
-const { beforeCreate } = require("./Blog");
 
 class User extends Model {
   checkPassword(loginPw) {
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-//const routes = require("./controllers");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require("./utils/helpers");
@@ -31,7 +31,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-//app.use(routes);
+app.use(routes);
+
+app.get("/", (req, res) => {
+  res.render("post");
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
