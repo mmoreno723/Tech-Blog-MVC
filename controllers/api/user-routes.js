bdc11c97ecf8a2ad176d1bd0812bf7a8b267f91c
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-25 20:48:59 -0400

    user-routes added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,50 +1,43 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-// router.post("/", async (req, res) => {
-//   try {
-//     const userData = await User.create({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-//     req.session.save(() => {
-//       req.session.logged_in = true;
-
-//       res.status(200).json(userData);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+    req.session.save(() => {
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
+      res.status(400).json({ message: "Incorrect email or password" });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
+      res.status(400).json({ message: "Incorrect password" });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.logged_in = true;
+      req.session.loggedIn = true;
 
-      res.json({ user: userData, message: "You are now logged in!" });
+      res.json({ user: userData, message: "Successfully logged in" });
     });
   } catch (err) {
     res.status(400).json(err);
@@ -52,12 +45,30 @@ router.post("/login", async (req, res) => {
 });
 
 router.post("/logout", (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
     res.status(404).end();
+  }
+});
+
+router.delete("/user/:id", async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!userData) {
+      res
+        .status(404)
+        .json({ message: "Could not find the user with this ID." });
+      return;
+    }
+    res.json(userData);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
